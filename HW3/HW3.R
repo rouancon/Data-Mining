95e8546e23595bdb05eea06a07006405bfc365fb
rouancon <rouan.c@husky.neu.edu>
2019-02-23 16:13:00 -0500

    Task 4 abc complete

--- HW3/HW3.R
+++ HW3/HW3.R
@@ -48,6 +48,54 @@ qqPlot(fit, labels=row.names(concrete), id.method="identify", simulate=TRUE, mai
 
 library(car)
 par(mfrow=c(2,2))
-qqPlot(fit, labels=row.names(concrete), id.method="identify", simulate=TRUE, main="Q-Q Plot")
+crPlots(fit)
 
 #Part D
+
+#Task 4
+#Part A
+Forest_Fires_Data$Month <- NULL
+Forest_Fires_Data$Day <- NULL
+fires <- as.data.frame(Forest_Fires_Data)
+library(car)
+scatterplotMatrix(fires, spread=FALSE, lty.smooth=2, main="Scatter Plot Matrix")
+
+cor(fires)
+model <- lm(formula=Area ~., data=fires)
+summary(model)
+
+#Part B
+model <- lm(formula=Area ~ Rain + FFMC + DC, data=fires)
+summary(model)
+
+model <- lm(formula=Area ~ I(Rain^2) + FFMC + DC, data=fires)
+summary(model)
+
+plot(Forest_Fires_Data$Area, model$fitted.values)
+
+model <- lm(formula=Area ~ FFMC + DC, data=fires)
+summary(model)
+
+model <- lm(formula=Area ~ Rain + model$fitted.values, data=fires)
+summary(model)
+
+plot(Forest_Fires_Data$Area, model$fitted.values)
+
+#Part C
+#Typical Approach
+fit <- lm(formula=Area ~ Rain + FFMC + DC, data=fires)
+par(mfrow=c(2,2))
+plot(fit)
+
+#Enhanced Approach
+library(car)
+par(mfrow=c(1,1))
+fit <- lm(formula=Area ~ Rain + FFMC + DC, data=fires)
+qqPlot(fit, labels=row.names(fires), id.method="identify", simulate=TRUE, main="Q-Q Plot")
+
+library(car)
+par(mfrow=c(2,2))
+crPlots(fit)
+
+#Part D
+
