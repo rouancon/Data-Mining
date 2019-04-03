0cf3cbc01e44baab61a4ba4a35cad4a304f301cb
rouancon <rouan.c@husky.neu.edu>
2019-04-03 16:29:55 -0400

    Troubleshooting

--- HW5/HW5.r
+++ HW5/HW5.r
@@ -1,7 +1,7 @@
 #HW 5
 #Connor Rouan and Yadukrishnan Sethumadhavan
 
-#Problem 1
+#-- Problem 1 --
 #Part A (pre-processing)
 library(readxl)
 toyota <- read_excel("Documents/GitHub/DataMining-HW1/Data-Mining/HW5/ToyotaCorolla.xlsx", sheet = "data")
@@ -20,20 +20,16 @@ toyota$Color <- factor(toyota$Color)
 toyota<- toyota[c("Price","Age_08_04","KM","Fuel_Type","HP","Automatic","Doors","Quarterly_Tax","Mfr_Guarantee",
                   "Guarantee_Period","Airco","Automatic_airco","CD_Player","Powered_Windows","Sport_Model",
                   "Tow_Bar")]
+
 #converting to dummy variable
 library(caret)
 dmy <- dummyVars("~.", data = toyota,fullRank = F)
 toyota_d <- data.frame(predict(dmy, newdata = toyota))
 
-#normalizing
-normalize <- function(x) {
-  return ((x - min(x)) / (max(x) - min(x))) }
-toyota_n <- as.data.frame(lapply(toyota_d, normalize))
-
 #splitting
-train_toyota<- toyota_n[1:718,]
-val_toyota<- toyota_n[719:1149,]
-test_toyota<-toyota_n[1149:1436,]
+train_toyota<- toyota[1:718,]
+val_toyota<- toyota[719:1149,]
+test_toyota<-toyota[1149:1436,]
 
 
 #Part A, i
@@ -51,25 +47,29 @@ text(opt_model)
 
 #Part A, ii
 #predict the training data sets
-pred <- predict(opt_model, newdata=train_toyota, type = c("vector"))
+train-predict <- train_toyota
+train-predict$Price <- NULL
+pred <- predict(opt_model, newdata=train_toyota, type = "vector")
 #Examine results
 library(Metrics)
 rmse(actual = train_toyota$price, predicted = pred)
 
 #predict the validation data sets
-pred <- predict(opt_model, newdata=val_toyota, type = c("vector"))
+pred <- predict(opt_model, newdata=val_toyota, type = "vector")
 #Examine results
 rmse(actual = val_toyota$price, predicted = pred)
 
 #prediction data set
-pred <- predict(opt_model, newdata=test_toyota, type = c("vector"))
+pred <- predict(opt_model, newdata=test_toyota, type = "vector")
 #Examine results
 rmse(actual = test_toyota$price, predicted = pred)
 
 
 #Part A, iii
 #predict the validation data sets on non-pruned model
-pred <- predict(model, newdata=, type = c("vector"))
+pred <- predict(model, newdata=, type = "vector")
 #Examine results
 rmse(actual = val_toyota$price, predicted = pred)
 
+
+#-- Problem 2 --
