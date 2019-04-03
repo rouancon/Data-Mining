b0bc6c2b1d7c42fd40bda9d752dc75b01c3e249b
rouancon <rouan.c@husky.neu.edu>
2019-04-03 11:07:25 -0400

    Combined

--- HW5/HW5.r
+++ HW5/HW5.r
@@ -2,7 +2,7 @@
 #Connor Rouan and Yadukrishnan Sethumadhavan
 
 #Problem 1
-
+#Part A (pre-processing)
 library(readxl)
 toyota <- read_excel("ToyotaCorolla.xlsx", sheet = "data")
 View(toyota)
@@ -31,6 +31,48 @@ normalize <- function(x) {
 toyota_n <- as.data.frame(lapply(toyota_d, normalize))
 
 #splitting
-traintoyota<- toyota_n[1:718,]
-valtoyota<- toyota_n[719:1149,]
-testtoyota<-toyota_n[1149:1436,]
+train_toyota<- toyota_n[1:718,]
+val_toyota<- toyota_n[719:1149,]
+test_toyota<-toyota_n[1149:1436,]
+
+
+#Part A, i
+#build the model
+library(rpart)
+model <- rpart(price~Age_08_04+KM+Fuel_Type+HP+Automatic+Doors+Quarterly_Tax+Mfg_Guarantee+Guarantee_Period+Airco+Automatic_Airco+CD_Player+Powered_Windows+Sport_Model+Tow_Bar, data=train_toyota, method=anova)
+printcp(model)
+#refine the model
+opt_model <- prune(model, cp=)
+#results of the model
+rsq.rpart(opt_model)
+print(opt_model)
+plot(opt_model)
+text(opt_model)
+
+
+#Part A, ii
+#predict the training data sets
+pred <- predict(opt_model, newdata=train_toyota, type = c("vector"))
+#Examine results
+library(Metrics)
+rmse(actual = train_toyota$price, predicted = pred)
+
+#predict the validation data sets
+pred <- predict(opt_model, newdata=val_toyota, type = c("vector"))
+#Examine results
+library(Metrics)
+rmse(actual = val_toyota$price, predicted = pred)
+
+#prediction data set
+pred <- predict(opt_model, newdata=predict_toyota, type = c("vector"))
+#Examine results
+library(Metrics)
+rmse(actual = predict_toyota$price, predicted = pred)
+
+
+#Part A, iii
+#predict the validation data sets on non-pruned model
+pred <- predict(model, newdata=, type = c("vector"))
+#Examine results
+library(Metrics)
+rmse(actual = val_toyota$price, predicted = pred)
