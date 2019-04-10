b5e9cb8322ece47cd495238fc3d72a0ea25e26a3
sethumadhavany <46494505+sethumadhavany@users.noreply.github.com>
2019-04-10 10:38:14 -0400

    Update HW6.r

--- HW6/HW6.r
+++ HW6/HW6.r
@@ -4,6 +4,27 @@
 #--Problem 1--
 #Part A
 
+library(readxl)
+toyota <- read_excel("18 Toyota Corolla.xlsx")
+View(toyota)
+
+#converting Fule_Type to categorical variable
+toyota$Fuel_Type <- factor(toyota$Fuel_Type)
+toyota$Fuel_Type <- as.numeric(toyota$Fuel_Type)
+toyota$Fuel_Type <- factor(toyota$Fuel_Type)
+
+toyota<- toyota[c("Price","Age_08_04","KM","Fuel_Type","HP","Automatic","Doors","Quarterly_Tax","Mfr_Guarantee",
+                  "Guarantee_Period","Airco","Automatic_airco","CD_Player","Powered_Windows","Sport_Model",
+                  "Tow_Bar")]
+
+#converting to dummy variable
+library(caret)
+dmy <- dummyVars("~.", data = toyota,fullRank = F)
+toyota_d <- data.frame(predict(dmy, newdata = toyota))
+
+#splitting
+train_toyota<- toyota_d[1:1077,]
+val_toyota<- toyota_d[1077:1436,]
 
 model <- neuralnet(
           formula = Price~Age_08_04+KM+Fuel_Type.1+Fuel_Type.2+Fuel_Type.3+HP+Automatic+Doors+Quarterly_Tax+Mfr_Guarantee+Guarantee_Period+Airco+Automatic_airco+CD_Player+Powered_Windows+Sport_Model+Tow_Bar, 
